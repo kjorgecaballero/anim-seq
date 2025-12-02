638dfb28af1892f662ba06bb1c23be9086de6723
kjorgecaballero <kja.cruzcaballero@gmail.com>
2025-12-02 12:43:25 -0600

    Added button to export a sequence

--- ui/menus.py
+++ ui/menus.py
@@ -8,9 +8,18 @@ def menu_func_import(self, context):
     )
 
 
+def menu_func_export(self, context):
+    self.layout.operator(
+        "export_scene.meshseq", 
+        text="Mesh Sequence (.fbx, .obj)"
+    )
+
+
 def register():
     bpy.types.TOPBAR_MT_file_import.append(menu_func_import)
+    bpy.types.TOPBAR_MT_file_export.append(menu_func_export)
 
 
 def unregister():
     bpy.types.TOPBAR_MT_file_import.remove(menu_func_import)
+    bpy.types.TOPBAR_MT_file_export.remove(menu_func_export)
