ddb654bf34afe17f8db7f3b55f2c9cea8beb5e00
kjorgecaballero <kja.cruzcaballero@gmail.com>
2025-12-04 18:20:37 -0600

    Updated version and Removed warning

--- __init__.py
+++ __init__.py
@@ -1,11 +1,10 @@
 bl_info = {
     "name": "anim_sequence_io",
     "author": "Siruka",
-    "version": (0, 0, 1),  
+    "version": (1, 0, 0),  
     "blender": (4, 0, 0),
     "location": "File > Import",
     "description": "Import mesh sequences (FBX/OBJ) as shapekeys with animation",
-    "warning": "Early alpha version - API may change",
     "category": "Import-Export",
 }
 
